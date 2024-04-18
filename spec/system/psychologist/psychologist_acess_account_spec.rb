6cb855756b9fbe1f672bb85d236c86ecad397e2a
Moises Almeida <moisesssprg@gmail.com>
2024-04-17 23:38:58 -0300

    feat: add log out button

--- spec/system/psychologist/psychologist_acess_account_spec.rb
+++ spec/system/psychologist/psychologist_acess_account_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 describe 'Psicologo acessa a plataforma' do
   it 'e é autenticado com sucesso' do
+    psychologist = Psychologist.create!(email: 'luis@psicologo.com', password: '123456')
+    
     visit root_path
     click_on 'Entrar como Psicólogo'
-
-    fill_in 'Email', with: 'usuario@email.com'
+    fill_in 'E-mail', with: 'luis@psicologo.com'
     fill_in 'Senha', with: '123456'
     click_on 'Entrar'
 
     expect(page).to have_content 'Login efetuado com sucesso.'
-    within '#navbar' do
+    within 'nav' do
       expect(page).to have_button 'Sair'
-      expect(page).to have_content 'usuario@email.com'
+      expect(page).to have_content 'luis@psicologo.com'
     end
   end
   
