4fdcdb3cce437d5a904f67c8951e021b8167f527
Moises Almeida <moisesssprg@gmail.com>
2024-04-17 23:45:44 -0300

    test: adds test scenario for when psychologist can't start session

--- spec/system/psychologist/psychologist_acess_account_spec.rb
+++ spec/system/psychologist/psychologist_acess_account_spec.rb
@@ -16,5 +16,16 @@ describe 'Psicologo acessa a plataforma' do
       expect(page).to have_content 'luis@psicologo.com'
     end
   end
+
+  it 'e não é autenticado' do
+    psychologist = Psychologist.create!(email: 'luis@psicologo.com', password: '123456')
+
+    visit new_psychologist_session_path
+    fill_in 'E-mail', with: 'jorginho@gmail.com'
+    fill_in 'Senha', with: '328902'
+    click_on 'Entrar'
+
+    expect(page).to have_content 'E-mail ou senha inválidos.'
+  end
   
 end
