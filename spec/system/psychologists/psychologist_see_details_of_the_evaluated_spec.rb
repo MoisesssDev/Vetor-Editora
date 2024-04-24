f5a240f1c2bc375a5ac4053a8fb3bcfdd72b941f
Moises Almeida <moisesssprg@gmail.com>
2024-04-24 13:38:25 -0300

    test: Adds test scenario for when no instruments are applied

--- spec/system/psychologists/psychologist_see_details_of_the_evaluated_spec.rb
+++ spec/system/psychologists/psychologist_see_details_of_the_evaluated_spec.rb
@@ -2,25 +2,38 @@ require 'rails_helper'
 
 describe 'Psicologo visualiza detalhes do avaliado' do
 
-  it 'com sucesso' do
-    psychologist = Psychologist.create!(email: 'luis@psicologo.com', password: '123456')
-    evaluated = psychologist.evaluateds.create!(name: 'Fernando', cpf: '12345678901', email: 'fernando@paciente.com',
-                                    birthdate: '01/01/1980')
-    instrument = Instrument.create!(name: 'Instrumento 1', description: 'Descrição do instrumento 1')
-    instrument.instruments_applied.create!(evaluated: evaluated, status: :not_started)
-            
-    login_as psychologist
-    visit root_path
-    click_on 'Fernando'
+  context 'com sucesso' do
+    it 'visualiza detalhes do avaliado' do
+      psychologist = Psychologist.create!(email: 'luis@psicologo.com', password: '123456')
+      evaluated = psychologist.evaluateds.create!(name: 'Fernando', cpf: '12345678901', email: 'fernando@paciente.com',
+                                      birthdate: '01/01/1980')
+      instrument = Instrument.create!(name: 'Instrumento 1', description: 'Descrição do instrumento 1')
+      instrument.instruments_applied.create!(evaluated: evaluated, status: :not_started)
+              
+      login_as psychologist
+      visit root_path
+      click_on 'Fernando'
 
-    expect(page).to have_content 'Nome: Fernando'
-    expect(page).to have_content 'CPF: 12345678901'
-    expect(page).to have_content 'E-mail: fernando@paciente.com'
-    expect(page).to have_content 'Data de nascimento: 01/01/1980'
-    expect(page).to have_content 'Instrumentos aplicados:'
-    expect(page).to have_content 'Instrumento 1'
-    expect(page).to have_content 'Descrição do instrumento 1'
-    expect(page).to have_content 'Status: Não iniciado'
+      expect(page).to have_content 'Nome: Fernando'
+      expect(page).to have_content 'CPF: 12345678901'
+      expect(page).to have_content 'E-mail: fernando@paciente.com'
+      expect(page).to have_content 'Data de nascimento: 01/01/1980'
+      expect(page).to have_content 'Instrumentos aplicados:'
+      expect(page).to have_content 'Instrumento 1'
+      expect(page).to have_content 'Descrição do instrumento 1'
+      expect(page).to have_content 'Status: Não iniciado'
+    end
+
+    it 'sem instrumentos aplicados' do
+      psychologist = Psychologist.create!(email: 'luis@psicologo.com', password: '123456')
+      evaluated = psychologist.evaluateds.create!(name: 'Fernando', cpf: '12345678901', email: 'fernando@paciente.com',
+                                      birthdate: '01/01/1980')
+              
+      login_as psychologist
+      visit evaluated_path(evaluated)
+
+      expect(page).to have_content 'Nenhum instrumento aplicado'
+    end
   end
 
 end
